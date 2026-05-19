bbc57f79655489036c2de1a765db4c86072581cf
Pham Tien Thang <>
2026-05-20 00:11:12 +0700

    cân bằng game

--- lib/game/scene_level/scene_1/worm_journey_game_scene1.dart
+++ lib/game/scene_level/scene_1/worm_journey_game_scene1.dart
@@ -101,7 +101,7 @@ class WormJourneyGame extends FlameGame
   static const double _level3PlayerMoveIntervalScale = 1.08;
   static const int _greenBossLength = 8;
   static const double _greenBossSpeedUnitIntervalScale = 0.1;
-  static const int _greenBossBaseSpeedUnits = 1;
+  static const double _greenBossBaseSpeedUnits = 0.5;
   static const int _level5GreenBossSpeedLagUnits = 7;
   static const int _pineappleBaseHardness = 25;
   static const int _greenBossHitSlowUnits = 3;
